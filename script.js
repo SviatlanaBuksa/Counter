484596247270594e1406a158175a22e200a02a74
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-08-22 16:47:46 +0200

    decrease function created

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+"use strict";
 
+let decreaseBtn = document.getElementById("decrease-btn");
+let counterValue = document.getElementById("counter-value");
+let currentValue = +counterValue.innerHTML;
+
+console.log(currentValue);
+
+decreaseBtn.addEventListener("click", decreaseValue);
+
+function decreaseValue() {
+  counterValue.innerHTML = `${--currentValue}`;
+}
