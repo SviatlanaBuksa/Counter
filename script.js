6a15ba126b0784a995e24f12529d319e4640713b
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-08-23 14:00:01 +0200

    Increase and reset functionality done

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 "use strict";
 
-let decreaseBtn = document.getElementById("decrease-btn");
 let counterValue = document.getElementById("counter-value");
 let currentValue = +counterValue.innerHTML;
 
-console.log(currentValue);
-
+let decreaseBtn = document.getElementById("decrease-btn");
 decreaseBtn.addEventListener("click", decreaseValue);
 
+let increaseBtn = document.getElementById("increase-btn");
+increaseBtn.addEventListener("click", increaseValue);
+
+let resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", resetValue);
 function decreaseValue() {
   counterValue.innerHTML = `${--currentValue}`;
+  setCurrentValueColor(currentValue);
+}
+function increaseValue() {
+  counterValue.innerHTML = `${++currentValue}`;
+  setCurrentValueColor(currentValue);
+}
+function resetValue() {
+  currentValue = 0;
+  counterValue.innerHTML = `${currentValue}`;
+  setCurrentValueColor(currentValue);
+}
+
+function setCurrentValueColor(value) {
+  if (value < 0) counterValue.style.color = "#FF0000";
+  else if (value > 0) {
+    counterValue.style.color = "#00FF00";
+  } else {
+    counterValue.style.color = "#000";
+  }
 }
